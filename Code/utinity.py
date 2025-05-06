6448aa96907e5635660fb07040919b2c8da150a8
cqdyf099 <51054466+cqdyf099@users.noreply.github.com>
2025-05-06 21:44:27 +0800

    Add files via upload

--- Code/utinity.py
+++ Code/utinity.py
@@ -1,14 +1,12 @@
 # !/usr/bin/env python
 # coding: utf-8
 # version：2024.08.12
-# yongqiang cai
 
 import re
 import torch
 import numpy as np
 
 import sys
-# sys.path.append("/home/yqcai/lightmann") # ~/lightmann does not work?
 sys.path.append(".")
 
 
@@ -34,9 +32,6 @@ def find_all_substring_positions(text, substring):
     return positions
  
 # 找出词在 token 序列中的位置
-# 本来可以用 find_all_substring_positions 来把代码写得觉得一些，先用 DYF 之前的版本 word_in_sentence_tk_all
-# '综合性医疗网站，提供妇产科小儿科内科以及外科资讯和讨论。'.find('大')
-# ''.join(sentence_list).find('大')
 def get_index_of_word_in_tokens(target_word, tokens):
     '''
     Example:
@@ -101,24 +96,3 @@ def distance_matrix(vs):
     n = vs.shape[0]
     M = [[torch.linalg.norm(vs[i] - vs[j]) for i in range(n)] for j in range(n)]
     return torch.from_numpy(np.array(M))
-
-# def distance_matrix(vs):
-#     '''
-#     e.g. input: vs.shape = torch.Size([4, 4096])
-#     out.shape = torch.Size([4, 4])
-#     '''
-#     # 计算两两向量之间的欧氏距离，利用广播机制
-#     # vs.unsqueeze(0) 使得 vs 的维度变为 (1, n, d)
-#     # vs.unsqueeze(1) 使得 vs 的维度变为 (n, 1, d)
-#     # 然后利用广播，直接计算 (n, n, d) 的张量
-#     diff = vs.unsqueeze(0) - vs.unsqueeze(1)
-    
-#     # 计算每对向量的欧氏距离，沿着最后一个维度求范数
-#     dist_matrix = torch.linalg.norm(diff, dim=-1)
-    
-#     return dist_matrix
-
- 
-
-
-
